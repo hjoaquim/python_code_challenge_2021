94f93c36f5b6c28b17170be6f53e3c33725b4d36
hjoaquim <h.joaquim@campus.fct.unl.pt>
2022-04-27 21:21:18 +0100

    test with api key commented

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 To get an API_KEY go to: https://www.omdbapi.com/apikey.aspx
 NOTE iddeally, this file should not be commited or if commited not publicly available.
 """
-API_KEY = "YOUR_API_KEY"
+
+API_KEY = "api key here"
